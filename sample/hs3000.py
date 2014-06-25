01f4b7e50fb832d30c84c3788445861b858a9020
jemyzhang <jemy.zhang@gmail.com>
2014-06-25 10:53:00 +0800

    rename some of the keynames and create a sample hs pdb

--- sample/hs3000.py
+++ sample/hs3000.py
@@ -15,17 +15,17 @@ keys = [
     ('1', 0x01), ('2', 0x02), ('3', 0x03), ('4', 0x04),
     ('5', 0x05), ('6', 0x06), ('7', 0x07), ('8', 0x08),
     ('9', 0x09), ('0', 0x00),
-    ('Recall', 0x0b),
-    ('*', 0x0c),
+    ('Previous Ch', 0x0b),
+    ('Info', 0x0c),
     ('Power On', 0x0d),
     ('Mute', 0x0e),
     ('Freeze', 0x0f),
-    ('Pic Mode', 0x10),
-    ('Sound Mode', 0x11),
+    ('Mode', 0x10),
+    ('Surround', 0x11),
     ('Input', 0x12),
     ('Zoom', 0x13),
     ('Menu', 0x14),
-    ('Ok', 0x15),
+    ('OK', 0x15),
     ('Up', 0x16),
     ('Down', 0x17),
     ('Right', 0x18),
@@ -38,7 +38,7 @@ keys = [
     ('Subtitle', 0x1f),
     ('Vol+', 0x44),
     ('Vol-', 0x43),
-    ('Back', 0x48),
+    ('Cancel', 0x48),
     ('Language', 0x49),
     ('Ch+', 0x4a),
     ('Ch-', 0x4b),
@@ -65,17 +65,17 @@ custfact = 0xfc00
 keyfacts = [
     ('fLeave', 0x47),
     ('fMac', 0x0a),
-    ('fMode', 0x00),
+    ('Setup', 0x00),
     ('fBalance', 0x06),
     ('fAdc', 0x40),
     ('fScreenTest', 0x58),
     ('fBurnIn', 0x15),
     ('fSavingMode', 0x60),
     ('fIP', 0x4e),
-    ('fPC', 0x18),
-    ('fHDMI', 0x52),
-    ('fComponent', 0x01),
-    ('fAV', 0x05),
+    ('PC', 0x18),
+    ('HDMI', 0x52),
+    ('Component', 0x01),
+    ('AV', 0x05),
     ('fF1', 0x5d),
     ('fF2', 0x24),
     ('fF3', 0x56),
@@ -88,7 +88,7 @@ keyfacts = [
 if __name__ == '__main__':
     pronto = ProntoConv()
     novii = Novii()
-    novii.setpdb('sample.pdb')
+    novii.setpdb('sample_hs.pdb')
     for n, k in keys:
         prontocode = pronto.conv(customcode, k)
         noviidata = novii.conv(prontocode)
@@ -96,7 +96,7 @@ if __name__ == '__main__':
         novii.addrec(rec)
     # factory
     for n, k in keyfacts:
-        prontocode = pronto.conv(customcode, k)
+        prontocode = pronto.conv(custfact, k)
         noviidata = novii.conv(prontocode)
         rec = novii.makerec(noviidata, n)
         novii.addrec(rec)
